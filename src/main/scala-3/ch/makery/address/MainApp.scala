1e669752fcce142cf5f8a60ece6d562602dc95ca
Gow Rui Jiun <ruijiun@gmail.com>
2025-06-30 17:46:10 +0800

    30/6/2025

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,16 +1,33 @@
 package ch.makery.address
 
+import ch.makery.address.model.Person
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
+import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
 
 object MainApp extends JFXApp3:
 
   //Window Root Pane
   var roots: Option[scalafx.scene.layout.BorderPane] = None
+
+  val personData = new ObservableBuffer[Person]()
+  /**
+   * Constructor
+   */
+  personData += new Person("Hans", "Muster")
+  personData += new Person("Ruth", "Mueller")
+  personData += new Person("Heinz", "Kurz")
+  personData += new Person("Cornelia", "Meier")
+  personData += new Person("Werner", "Meyer")
+  personData += new Person("Lydia", "Kunz")
+  personData += new Person("Anna", "Best")
+  personData += new Person("Stefan", "Meier")
+  personData += new Person("Martin", "Mueller")
 
   override def start(): Unit =
     // transform path of RootLayout.fxml to URI for resource location.
@@ -37,3 +54,25 @@ object MainApp extends JFXApp3:
     loader.load()
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     this.roots.get.center = roots
+
+  val stringA = new StringProperty("sunway")  //publisher
+  val stringB = new StringProperty("monash")  // subscriber
+  val stringC = new StringProperty("taylor")  // subcriber
+
+  stringA.onChange((a, b, c) => {
+    println("a has changed value from " + b + " to " + c)
+  })
+
+  stringB <== stringA
+  stringC <==> stringA
+  stringA.value = "segi"
+  stringC.value = "utm"
+
+  println("string a value: " + stringA.value)
+  println("string b value: " + stringB.value)
+  println("string c value: " + stringC.value)
+
+  stringA.value = "monash"
+
+  val add: (Int, Int) => Int = (a, b) => { a + b }
+  println(add (1,2))
